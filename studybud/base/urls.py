25490fd7ed3b29e4152aab5e134cdb31326a92cd
Hari Sirikonda <harisirikonda22@gmail.com>
2026-08-16 21:01:52 +0530

    Added the registration functionality

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('login', views.loginPage, name="login"),
+    path('register', views.registerPage, name="register"),
     path('logout', views.logoutUser, name="logout"),
     path('room/<str:pk>', views.room, name="room"),
     path('create-room/', views.cerateRoom, name="create-room"),
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 from django.contrib import messages
@@ -16,12 +17,12 @@ rooms = [
 ]
 
 def loginPage(request):
-
+    page = "login"
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -37,12 +38,30 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or Password Does not Exist!')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = "register"
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An Error occured during registraion.")
+
+    context = {'page' : page, 'form' : form}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
