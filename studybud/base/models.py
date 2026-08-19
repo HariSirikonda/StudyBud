24e64c3f8d3a5f10bda3dbbc8a8e7eb8220df5c6
Hari Sirikonda <harisirikonda22@gmail.com>
2026-08-20 04:20:41 +0530

    Added the participants tab to the room page

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -12,6 +12,7 @@ class Topic(models.Model):
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic  = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     updated = models.DateTimeField(auto_now=True)
